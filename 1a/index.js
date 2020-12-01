2065a3ec6f2934ddfa5bdaf3b5da6447956d0af4
James Billingham <git@jamesbillingham.com>
2020-12-01 15:06:36 +0000

    1a

---
+++ 1a/index.js
@@ -0,0 +1,18 @@
+import fs from 'fs/promises';
+
+const inputRaw = await fs.readFile('./input', 'utf8');
+
+const lines = inputRaw
+	.split('\n')
+	.filter(Boolean)
+	.map(i => parseInt(i, 10));
+
+a:
+for (const n1 of lines) {
+	for (const n2 of lines) {
+		if (n1 + n2 === 2020) {
+			console.log(n1 * n2);
+			break a;
+		}
+	}
+}
