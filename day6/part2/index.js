5577c4599fd5e38903eb126403a75234d098ec24
James Billingham <git@jamesbillingham.com>
2020-12-06 12:31:01 +0000

    improved implementation

--- day6/part2/index.js
+++ day6/part2/index.js
@@ -2,19 +2,25 @@ import fs from 'fs/promises';
 
 const inputRaw = await fs.readFile('../input', 'utf8');
 
+const fullSet = new Set(
+	new Array(26)
+		.fill(null)
+		.map((_, i) => String.fromCharCode(65 + i))
+);
+
 const sum = inputRaw
+	.toUpperCase()
 	.trim()
 	.split('\n\n')
 	.map(g => g.split('\n'))
-	.map(g =>
-		Object.values(g
-			.map(l => [...new Set(l.split(''))])
-			.flat()
-			.reduce((acc, cur) => ({
-				...acc,
-				[cur]: (acc[cur] ?? 0) + 1,
-			}), {})
-		).filter(v => v === g.length).length
+	.map(g => g
+		.map(l => new Set(l.split('')))
+		.reduce(intersect, fullSet)
+		.size
 	).reduce((acc, cur) => acc + cur, 0);
 
 console.log(sum);
+
+function intersect(a, b) {
+	return new Set([...a].filter(x => b.has(x)))
+}
